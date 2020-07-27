3887f9ce1df55a21e651104e6caf5ba98182da8a
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-27 16:57:35 -0500

    all answered except 10

--- assignment.sql
+++ assignment.sql
@@ -19,20 +19,36 @@ JOIN "country" ON "country".region_id = "region".id
 GROUP BY "region".name;
 
 -- 5. Displays the total population for each region.
-
+SELECT "region".name, SUM("population") FROM "region" 
+JOIN "country" ON "country".region_id = "region".id
+GROUP BY "region".name;
 
 -- 6. Display the name and sq km for the 10 countries with the most land area (sort by land area).
-
+SELECT "country".name, "land_area".sq_km FROM "country"
+JOIN "land_area" ON "country".country_code="land_area".country_code
+ORDER BY "land_area".sq_km DESC
+LIMIT 10;
 
 -- Stretch Mode
 
 -- 7. Display the total land area for each region sorted by land area (requires two joins). 
-
+SELECT "region".name, SUM("land_area".sq_km) FROM "region" 
+JOIN "country" ON "country".region_id = "region".id
+JOIN "land_area" ON "country".country_code="land_area".country_code
+GROUP BY "region".name;
 
 -- 8. Display the top 10 countries with the highest population density (greatest population per sq km).
+SELECT "country".name, "country".population/"land_area".sq_km FROM "country"
+JOIN "land_area" ON "country".country_code="land_area".country_code
+ORDER BY "country".population/"land_area".sq_km DESC
+LIMIT 10;
 
 
 -- 9. Display the population density for each region (sort by population density).
 
+SELECT "region".name, SUM("country".population/"land_area".sq_km) FROM "region" 
+JOIN "country" ON "country".region_id = "region".id
+JOIN "land_area" ON "country".country_code="land_area".country_code
+GROUP BY "region".name;
 
 -- 10. Display the population density for the 10 countries with the largest land area.
